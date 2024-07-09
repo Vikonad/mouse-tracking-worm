77f97b346c021427a0afc675ebfc5d4581b8d281
Ārash Momeni <133092049+Vikonad@users.noreply.github.com>
2024-07-09 11:13:26 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,69 @@
+import arcade, math
+from screeninfo import get_monitors
+
+# Get the primary monitor's width and height
+monitor = get_monitors()[0]
+SCREEN_WIDTH = monitor.width
+SCREEN_HEIGHT = monitor.height
+
+SCREEN_TITLE = "3D snake"
+
+class Circle:
+    def __init__(self, tx, ty, size, color, speed, angle):
+        self.x, self.y = 0, 0
+        self.tx, self.ty = tx, ty
+        self.size = size
+        self.color = color
+        self.speed = speed
+        self.angle = angle
+
+    def draw(self):
+        
+        # Moving squares smoothly
+        self.x += (self.tx - self.x) / self.speed
+        self.y += (self.ty - self.y) / self.speed
+
+        # Drawing squares
+        arcade.draw_rectangle_filled(self.x, self.y, self.size, self.size, self.color, self.angle)
+
+class MyGame(arcade.Window):
+    def __init__(self):
+        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE, fullscreen=True)
+        self.mousex = 0
+        self.mousey = 0
+        self.worm = [Circle(0, 300, 40, arcade.color.BLACK, 1, 0)]
+        for i in range(1, 101):
+            size = 50 - i % 15
+            color = (0, 0, 255 - i % 15 * 20 - i * 3)
+            speed = i / 1.5
+            self.worm.append(Circle(0, 300, size, color, speed, 0))
+        self.speedx, self.speedy = 1, 1
+
+    def on_draw(self):
+        arcade.start_render()
+
+        # Moving the worm
+        for segment in self.worm[::-1]:
+            
+            # Rotating the worm based on its speed
+            dist_x = self.mousex - segment.tx
+            dist_y = self.mousey - segment.ty
+            distance = math.sqrt(dist_x**2 + dist_y**2)
+            if distance > 0:
+                segment.angle -= (distance / 150) / 3
+
+            # Making the mouse position as the worm's target
+            segment.tx += dist_x / 50
+            segment.ty += dist_y / 50
+            segment.draw()
+
+    def on_mouse_motion(self, x, y, dx, dy):
+        self.mousex = x
+        self.mousey = y
+
+def main():
+    game = MyGame()
+    arcade.run()
+
+if __name__ == "__main__":
+    main()
